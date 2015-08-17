86ad58d08f1a972c909c854faa1625121d1f2147
shinnlove <306086640@qq.com>
2015-08-17 17:02:59 +0800

    Update common.php
    this file to help common function works.

--- common.php
+++ common.php
@@ -60,3 +60,122 @@
 		$media = array_merge( array('header' => $httpinfo), array('body' => $package) );
 		return $media;
 	}
+
+	/**
+	 * 不转义中文字符和\/的 json 编码方法
+	 * @param array $arr 待编码数组
+	 * @return string
+	 */
+	function jsencode($arr) {
+		$str = str_replace ( "\\/", "/", json_encode ( $arr ) );
+		$search = "#\\\u([0-9a-f]+)#ie";
+	
+		if (strpos ( strtoupper(PHP_OS), 'WIN' ) === false) {
+			$replace = "iconv('UCS-2BE', 'UTF-8', pack('H4', '\\1'))";//LINUX
+		} else {
+			$replace = "iconv('UCS-2', 'UTF-8', pack('H4', '\\1'))";//WINDOWS
+		}
+	
+		return preg_replace ( $search, $replace, $str );
+	}
+	
+	// 数据保存到文件
+	function data2file($filename, $arr=''){
+		if(is_array($arr)){
+			$con = var_export($arr,true);
+			$con = "<?php\nreturn $con;\n?>";
+		} else{
+			$con = $arr;
+			$con = "<?php\n $con;\n?>";
+		}
+		write_file($filename, $con);
+	}
+	
+	/**
+	 * 将standard obj转成array格式的函数
+	 * @param standard obj $obj
+	 */
+	function objtoarr($obj){
+		$ret = array();
+		foreach($obj as $key =>$value){
+			if(gettype($value) == 'array' || gettype($value) == 'object'){
+				$ret[$key] = objtoarr($value);
+			}
+			else{
+				$ret[$key] = $value;
+			}
+		}
+		return $ret;
+	}
+	
+	/**
+	 * 系统加密方法
+	 * @param string $data 要加密的字符串
+	 * @param string $key  加密密钥
+	 * @param int $expire  过期时间 单位 秒
+	 * @return string
+	 * @author winky
+	 */
+	function encrypt($data, $key = '', $expire = 0) {
+		$key  = md5(empty($key) ? C('DATA_AUTH_KEY') : $key);
+		$data = base64_encode($data);
+		$x    = 0;
+		$len  = strlen($data);
+		$l    = strlen($key);
+		$char = '';
+	
+		for ($i = 0; $i < $len; $i++) {
+			if ($x == $l) $x = 0;
+			$char .= substr($key, $x, 1);
+			$x++;
+		}
+	
+		$str = sprintf('%010d', $expire ? $expire + time():0);
+	
+		for ($i = 0; $i < $len; $i++) {
+			$str .= chr(ord(substr($data, $i, 1)) + (ord(substr($char, $i, 1)))%256);
+		}
+		return str_replace(array('+','/','='),array('-','_',''),base64_encode($str));
+	}
+	
+	/**
+	 * 系统解密方法
+	 * @param  string $data 要解密的字符串 （必须是encrypt方法加密的字符串）
+	 * @param  string $key  加密密钥
+	 * @return string
+	 * @author winky
+	 */
+	function decrypt($data, $key = ''){
+		$key    = md5(empty($key) ? C('DATA_AUTH_KEY') : $key);
+		$data   = str_replace(array('-','_'),array('+','/'),$data);
+		$mod4   = strlen($data) % 4;
+		if ($mod4) {
+			$data .= substr('====', $mod4);
+		}
+		$data   = base64_decode($data);
+		$expire = substr($data,0,10);
+		$data   = substr($data,10);
+	
+		if($expire > 0 && $expire < time()) {
+			return '';
+		}
+		$x      = 0;
+		$len    = strlen($data);
+		$l      = strlen($key);
+		$char   = $str = '';
+	
+		for ($i = 0; $i < $len; $i++) {
+			if ($x == $l) $x = 0;
+			$char .= substr($key, $x, 1);
+			$x++;
+		}
+	
+		for ($i = 0; $i < $len; $i++) {
+			if (ord(substr($data, $i, 1))<ord(substr($char, $i, 1))) {
+				$str .= chr((ord(substr($data, $i, 1)) + 256) - ord(substr($char, $i, 1)));
+			}else{
+				$str .= chr(ord(substr($data, $i, 1)) - ord(substr($char, $i, 1)));
+			}
+		}
+		return base64_decode($str);
+	}
