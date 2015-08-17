10ca918d0637c5dc163aa7f9b43b9b411200ab61
shinnlove <306086640@qq.com>
2015-08-17 16:59:45 +0800

    Create common.php

---
+++ common.php
@@ -0,0 +1,62 @@
+    /**
+	 * 发送HTTP请求方法，目前只支持CURL发送请求
+	 * @param  string $url    请求URL
+	 * @param  array  $params 请求参数
+	 * @param  string $method 请求方法GET/POST
+	 * @return array  $data   响应数据
+	 */
+	function http($url, $params, $method = 'GET', $header = array(), $multi = false){
+		$opts = array(
+				CURLOPT_TIMEOUT        => 30,
+				CURLOPT_RETURNTRANSFER => 1,
+				CURLOPT_SSL_VERIFYPEER => false,
+				CURLOPT_SSL_VERIFYHOST => false,
+				CURLOPT_HTTPHEADER     => $header
+		);
+	
+		/* 根据请求类型设置特定参数 */
+		switch(strtoupper($method)){
+			case 'GET':
+				$opts[CURLOPT_URL] = $url . '?' . http_build_query($params);
+				break;
+			case 'POST':
+				//判断是否传输文件
+				$params = $multi ? $params : http_build_query($params);
+				$opts[CURLOPT_URL] = $url;
+				$opts[CURLOPT_POST] = 1;
+				$opts[CURLOPT_POSTFIELDS] = $params;
+				break;
+			default:
+				throw new Exception('不支持的请求方式！');
+		}
+	
+		/* 初始化并执行curl请求 */
+		$ch = curl_init();
+		curl_setopt_array($ch, $opts);
+		$data  = curl_exec($ch);
+		$error = curl_error($ch);
+		curl_close($ch);
+		if($error) throw new Exception('请求发生错误：' . $error);
+		return  $data;
+	}
+	
+	/**
+	 * 从微信服务器下载多媒体文件的curl封装。
+	 * @param string $url	url地址
+	 * @param unknown $params	参数
+	 * @return array	返回多媒体信息数组
+	 */
+	function downloadWeixinFile($url='', $params=array()){
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_URL, $url . '?' . http_build_query($params));
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		curl_setopt($ch, CURLOPT_NOBODY, 0);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		$package = curl_exec($ch);
+		$httpinfo = curl_getinfo($ch);
+		curl_close($ch);
+		$media = array_merge( array('header' => $httpinfo), array('body' => $package) );
+		return $media;
+	}
